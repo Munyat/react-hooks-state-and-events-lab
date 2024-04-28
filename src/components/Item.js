8dcbf5ee002649f435185a893b26c74f5c35dfe9
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2024-04-28 20:37:28 +0300

    state and events lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [addItem, setAddItem] = useState(false);
+  function addRemove() {
+    setAddItem((addItem) => !addItem);
+  }
+  const itemClass = addItem ? "in-cart" : "";
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addRemove}>
+        {addItem ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
